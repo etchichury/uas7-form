4e4ef3ef6e6bf2926264c979691d0ab889315227
etchichury <joaoetchichury@hotmail.com>
2020-06-01 16:12:32 -0300

    Add color constants

---
+++ uas7/lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const  primaryWhiteColor = Colors.white;
+const  secondaryWhiteColor = Colors.white70;
+const  primaryColor = const Color(0xff2f2f2f);
+const  secondaryColor = const Color(0xe0f0cfaf);
+const  backgroundColor = const Color(0xff444444);
