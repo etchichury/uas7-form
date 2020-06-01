d50c209d215af7271f5446232745416b77933c40
etchichury <joaoetchichury@hotmail.com>
2020-06-01 16:15:30 -0300

    Move MainScreen code to a separate file
    This will make development and maintainability easier

--- uas7/lib/main.dart
+++ uas7/lib/main.dart
@@ -1,134 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'utils/colors.dart';
+import 'screens/main_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Dias', home: new MyHomePage(title: 'Dias'));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.title,
-          style: TextStyle(fontWeight: FontWeight.w300, fontSize: 35),
-        ),
-        actions: [
-          IconButton(
-              icon: Icon(
-                Icons.menu,
-                color: colors['primary_white'],
-                size: 30,
-              ),
-              onPressed: null)
-        ],
-        backgroundColor: Colors.transparent,
-      ),
-      body: Center(
-        child: ListView(
-          children: [
-            new DayResume(),
-            new DayResume(),
-            new DayResume(),
-            new DayResume(),
-            new DayResume(),
-            new DayResume(),
-            new DayResume(),
-            new DayResume(),
-            new DayResume(),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-        backgroundColor: const Color(0xe0f0cfaf),
-      ),
-      backgroundColor: const Color(0xff444444),
-    );
-  }
-}
-
-class DayResume extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Text(
-            '29/03',
-            style: TextStyle(
-                fontWeight: FontWeight.w300, fontSize: 30, color: Colors.white),
-            textAlign: TextAlign.center,
-          ),
-          Divider(color: Colors.white),
-          Spacer(flex: 2),
-          Row(
-            children: [
-              Text(
-                'Urticas',
-                style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 25,
-                    color: Colors.white),
-                textAlign: TextAlign.left,
-              ),
-              Spacer(),
-              Text(
-                '0',
-                style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 25,
-                    color: Colors.white),
-                textAlign: TextAlign.right,
-              ),
-            ],
-          ),
-          Spacer(flex: 1),
-          Row(children: [
-            Text('Coceira',
-                style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 25,
-                    color: Colors.white)),
-            Spacer(),
-            Text('0',
-                style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 25,
-                    color: Colors.white)),
-          ])
-        ],
-      ),
-      padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
-      margin: const EdgeInsets.fromLTRB(25, 12, 25, 12),
-      height: 170,
-      decoration: BoxDecoration(
-        color: const Color(0xff2f2f2f),
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(spreadRadius: 0, blurRadius: 10, offset: Offset(2, 10))
-        ],
-      ),
-    );
+    return MaterialApp(title: 'UAS7 Form', home: new MainScreen(title: 'Dias'));
   }
 }
---
+++ uas7/lib/screens/main_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import 'package:uas7/utils/colors.dart';
+import 'package:uas7/components/day_overview.dart';
+import 'package:uas7/components/end_drawer.dart';
+
+class MainScreen extends StatefulWidget {
+  MainScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MainScreenSatate createState() => _MainScreenSatate();
+}
+
+class _MainScreenSatate extends State<MainScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: TextStyle(fontWeight: FontWeight.w300, fontSize: 35),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: primaryWhiteColor,
+              size: 30,
+            ),
+            onPressed: () => {_scaffoldKey.currentState.openEndDrawer()}
+          )
+        ],
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false
+      ),
+      body: Center(
+        child: ListView(
+          children: [
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+            new DayOverview(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Registrar dia',
+        child: Icon(Icons.add),
+        backgroundColor: secondaryColor,
+      ),
+      endDrawer: new EndDrawer(),
+      drawerEdgeDragWidth: 40,
+      backgroundColor: backgroundColor,
+    );
+  }
+}
