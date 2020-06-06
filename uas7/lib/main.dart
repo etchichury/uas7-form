e5210272a0f7595aeb5eb334219c7ad99cc24113
etchichury <joaoetchichury@hotmail.com>
2020-06-06 15:08:07 -0300

    Change MaterialApp title

--- uas7/lib/main.dart
+++ uas7/lib/main.dart
@@ -9,6 +9,8 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Formulário UAS7', 
       home: new MainScreen(),
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
