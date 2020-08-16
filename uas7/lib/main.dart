84fa2c4ca6da66ad105fa7019d1c3296926e01dc
etchichury <joaoetchichury@hotmail.com>
2020-08-16 02:39:34 -0300

    Add localization

--- uas7/lib/main.dart
+++ uas7/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'screens/main_screen.dart';
 
@@ -10,10 +11,15 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Formulário UAS7', 
+      title: 'Formulário UAS7',
       home: new MainScreen(),
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [const Locale("pt", "BR")],
     );
   }
 }
